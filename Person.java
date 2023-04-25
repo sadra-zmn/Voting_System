26673ba7a1a84f4f2310692e156761a97e24a563
Sadra.zmn <sdrzamani@gmail.com>
2023-04-25 17:31:37 +0430

    setter added

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    private String firstname ;
+    private String lastname ;
+
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
